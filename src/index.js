af9b6c07aa95b829bc906dfa20dfc1b49f1fbb9f
LilyMao <lily.mao@outlook.com>
2022-09-07 18:31:11 +0800

    second commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//if want to let conpoent render itself we change the state
+//state need to initialize
+//state can only be updated using 'setState'
+//constructor will only function as initia state
+class App extends React.Component{
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+   
+        state={lat: 40, errorMessage: ''};
+         //will be a number but not know now
+    
+
+// all dataloading will be in componentididmount
+    componentDidMount() {
+
+        window.navigator.geolocation.getCurrentPosition(
+            positon=>this.setState({lat: positon.coords.latitude}),
+            
+            err=>this.setState({errorMessage: err.message})
+
+        );
+
+       
+    }
+    // everytime a compoent is updated
+    componentDidUpdate(){
+        console.log('component was updated, it rerenderred')
+
+    }
+
+    renderContent(){
+        if(this.state.errorMessage&&!this.state.lat){
+            return <div>error: {this.state.errorMessage}</div>
+        }
+        if(this.state.lat &&!this.state.errorMessage){
+            return <SeasonDisplay lat={this.state.lat}/>
+        }
+        return <Spinner message='please accept location request'/>;
+        
+    }
+
+// constructor will first called when new compoent created
+//constructor is not only way to initiate state
+//every compoent need a render
+    render(){
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        )
+
+    
+        
+        
+}
+}
+ReactDOM.render(<App />,document.querySelector('#root'));
